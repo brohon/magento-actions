4ed76168501d20de583a2e7648fc93b26be53218
brohon <davehon1@gmail.com>
2022-08-27 13:31:04 -0500

    Removed refernces to magento directory.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,13 @@
 #!/bin/sh -l
 
-echo "hello your setup is $INPUT_PHP & $INPUT_PROCESS & $INPUT_OVERRIDE_SETTINGS"
-echo "This is a test of this script."
+echo "hello your setup is $INPUT_PHP & $INPUT_PROCESS"
 
-if [ $INPUT_PHP = 'auto' ]
+update-alternatives --set php /usr/bin/php${INPUT_PHP}
+
+if [ $INPUT_OVERRIDE_SETTINGS ]
 then
-  bash /opt/config/utils/php-compatibility-checker.sh
-else
-  echo "Forcing php to match specified php inputs argument"
-  update-alternatives --set php /usr/bin/php${INPUT_PHP}
-fi
-
-
-if [ "$INPUT_COMPOSER_VERSION" -ne 0 ]
-then
-  php7.2 /usr/local/bin/composer self-update --${INPUT_COMPOSER_VERSION}
-fi
-
-
-if [ $INPUT_OVERRIDE_SETTINGS = 1 ]
-then
-  [ -d config ] && ls ./config/*
-  [ -d scripts ] && ls ./scripts/*
-  [ -d config ] && cp -rf ./config/* /opt/config/
-  [ -d scripts ] && cp -rf ./scripts/* /opt/scripts/
+  cp -R ./config/* /opt/config/
+  cp -R ./scripts/* /opt/scripts/
   bash /opt/scripts/${INPUT_PROCESS}.sh
 else
   bash /opt/scripts/${INPUT_PROCESS}.sh
